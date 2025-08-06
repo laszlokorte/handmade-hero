c5bf86969653fcfdc60bfb667ac8d9b1960f05c0
Laszlo Korte <git@laszlokorte.de>
2025-08-07 00:04:23 +0200

    open window

--- build.bat
+++ build.bat
@@ -3,5 +3,5 @@
 mkdir build
 
 pushd build
-cl ../code/win32_handmade.cpp /LINKER user32.lib
+cl ../code/win32_handmade.cpp /LINKER user32.lib /LINKER Gdi32.lib
 popd
--- code/win32_handmade.cpp
+++ code/win32_handmade.cpp
@@ -1,7 +1,97 @@
 #include <windows.h>
+#include <wingdi.h>
+
+LRESULT CALLBACK MainWindowCallback(
+  HWND Window,
+  UINT Message,
+  WPARAM WParam,
+  LPARAM LParam
+)
+{
+    LRESULT Result = 0;
+    switch(Message) {
+        case WM_SIZE: {
+            OutputDebugString("WM_SIZE\n");
+        } break;
+
+        case WM_DESTROY: {
+            OutputDebugString("WM_DETROY\n");
+        } break;
+
+        case WM_CLOSE: {
+            OutputDebugString("WM_CLOSE\n");
+        } break;
+
+        case WM_ACTIVATEAPP: {
+            OutputDebugString("WM_ACTIVATEAPP\n");
+        } break;
+
+        case WM_PAINT: {
+           PAINTSTRUCT Paint;
+           HDC DeviceContext = BeginPaint(Window, &Paint);
+           int x = Paint.rcPaint.left;
+           int y = Paint.rcPaint.top;
+           int width = Paint.rcPaint.right - Paint.rcPaint.left;
+           int height = Paint.rcPaint.bottom - Paint.rcPaint.top;
+           static DWORD Color = WHITENESS;
+           PatBlt(DeviceContext, x, y, width, height, Color);
+           EndPaint(Window, &Paint);
+           OutputDebugString("WM_PAINT\n");
+           if(Color == WHITENESS) {
+               Color = BLACKNESS;
+           } else {
+               Color = WHITENESS;
+           }
+        } break;
+
+        default: {
+            Result = DefWindowProc(Window, Message, WParam, LParam);
+        } break;
+    }
+    return Result;
+
+}
 
 int CALLBACK
-WinMain(HINSTANCE hIntance, HINSTANCE nPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
-    MessageBox(0, "This is me", "Test", MB_OK|MB_ICONINFORMATION);
+WinMain(HINSTANCE Instance, HINSTANCE PrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+    // MessageBox(0, "This is me", "Test", MB_OK|MB_ICONINFORMATION);
+    WNDCLASS windowClass = {};
+    windowClass.style = CS_OWNDC|CS_HREDRAW|CS_VREDRAW;
+    windowClass.lpfnWndProc = *MainWindowCallback;
+    windowClass.hInstance = Instance;
+    // windowClass.hIcon = ;
+    windowClass.lpszClassName = "HandmadeHeroWindowClass";
+
+    if(RegisterClass(&windowClass)) {
+        HWND windowHandle = CreateWindowEx(
+            0,
+            windowClass.lpszClassName,
+            "Handmade Window",
+            WS_OVERLAPPEDWINDOW|WS_VISIBLE,
+            CW_USEDEFAULT,
+            CW_USEDEFAULT,
+            CW_USEDEFAULT,
+            CW_USEDEFAULT,
+            0,
+            0,
+            Instance,
+            0
+
+        );
+        if(windowHandle) {
+            MSG message;
+            for(;;) {
+               BOOL MessageResult = GetMessage(&message, 0, 0, 0);
+               if(MessageResult > 0) {
+                   TranslateMessage(&message);
+                   DispatchMessage(&message);
+               } else {
+                   break;
+               }
+            }
+        }
+    } else {
+
+    }
     return 0;
 }
