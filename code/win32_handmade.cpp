d9dbe60517b01967153a6de1e5ceb26b47490a81
Laszlo Korte <git@laszlokorte.de>
2025-08-07 14:28:06 +0200

    handle keyboard and gamepad input

--- code/win32_handmade.cpp
+++ code/win32_handmade.cpp
@@ -1,7 +1,8 @@
+#include <basetsd.h>
+#include <cstdint>
 #include <windows.h>
 #include <stdint.h>
-#include <wingdi.h>
-#include <winuser.h>
+#include <Xinput.h>
 
 #define local_persist static
 #define global_variable static
@@ -9,10 +10,10 @@
 
 global_variable bool Running;
 
-global_variable HCURSOR customCursor;
-
 struct game_state {
     uint32_t time;
+    int xpos;
+    int ypos;
 };
 
 global_variable game_state global_game_state = {};
@@ -27,6 +28,48 @@ struct win32_offscreen_buffer {
 
 global_variable win32_offscreen_buffer GlobalBackBuffer;
 
+struct win32_window_dimensions {
+    int width;
+    int height;
+};
+
+internal win32_window_dimensions Win32GetWindowSize(HWND Window) {
+    RECT ClientRect;
+    win32_window_dimensions dim;
+
+    GetClientRect(Window, &ClientRect);
+
+    dim.width = ClientRect.right - ClientRect.left;
+    dim.height = ClientRect.bottom - ClientRect.top;
+
+    return dim;
+};
+
+
+#define X_INPUT_GET_STATE(name) DWORD (WINAPI name)(DWORD wUserIndex, XINPUT_STATE* pState)
+#define X_INPUT_SET_STATE(name) DWORD (WINAPI name)(DWORD dwUserIndex, XINPUT_VIBRATION* pVibration)
+typedef X_INPUT_GET_STATE(x_input_get_state);
+typedef X_INPUT_SET_STATE(x_input_set_state);
+X_INPUT_GET_STATE(XInputGetStateStub)
+{
+    return 0;
+}
+X_INPUT_SET_STATE(XInputSetStateStub)
+{
+    return 0;
+}
+global_variable x_input_get_state *XInputGetState_ = XInputGetStateStub;
+global_variable x_input_set_state *XInputSetState_ = XInputSetStateStub;
+#define XInputGetState XInputGetState_
+#define XInputSetState XInputSetState_
+
+internal void Win32LoadXInput(void) {
+    HMODULE XInputLibrary = LoadLibrary("XInput1_4.dll");
+    if(XInputLibrary) {
+        XInputGetState = (x_input_get_state *) GetProcAddress(XInputLibrary, "XInputGetState");
+        XInputSetState = (x_input_set_state *) GetProcAddress(XInputLibrary, "XInputSetState");
+    }
+}
 internal void RenderGradient(win32_offscreen_buffer Buffer, int xoff, int yoff) {
     unsigned int* canvas = (unsigned int *) (Buffer.Memory);
     int cx = Buffer.width/ 2;
@@ -68,15 +111,12 @@ internal void ResizeDIBSection(win32_offscreen_buffer* buffer, int width, int he
     buffer->Info.bmiHeader.biClrImportant = 0;
     buffer->Memory = VirtualAlloc(0, allocBytes, MEM_COMMIT, PAGE_READWRITE);
 
-    RenderGradient(*buffer, global_game_state.time, global_game_state.time*2);
+    RenderGradient(*buffer, global_game_state.xpos, global_game_state.ypos);
 }
 
 
 
-internal void Win32DisplayBufferWindow(HDC DeviceContext, RECT ClientRect, win32_offscreen_buffer* Buffer, int x, int y, int width, int height) {
-    //StretchDIBits(DeviceContext, x, y, width, height, x, y, width, height, BitmapMemory, &BitmapInfo, DIB_RGB_COLORS, SRCCOPY);
-    int WindowWidth = ClientRect.right - ClientRect.left;
-    int WindowHeight = ClientRect.bottom - ClientRect.top;
+internal void Win32DisplayBufferWindow(HDC DeviceContext, int WindowWidth, int WindowHeight, win32_offscreen_buffer* Buffer) {
     StretchDIBits(
         DeviceContext,
         0, 0, WindowWidth, WindowHeight,
@@ -97,45 +137,66 @@ LRESULT CALLBACK Win32MainWindowCallback(
     LRESULT Result = 0;
     switch(Message) {
         case WM_SIZE: {
-            RECT ClientRect;
-            GetClientRect(Window, &ClientRect);
-            int width = ClientRect.right - ClientRect.left;
-            int height = ClientRect.bottom - ClientRect.top;
-            ResizeDIBSection(&GlobalBackBuffer, width, height);
-            OutputDebugString("WM_SIZE\n");
+            win32_window_dimensions WinSize = Win32GetWindowSize(Window);
+            ResizeDIBSection(&GlobalBackBuffer, WinSize.width, WinSize.height);
+            OutputDebugStringA("WM_SIZE\n");
         } break;
 
         case WM_DESTROY: {
             Running = false;
-            OutputDebugString("WM_DETROY\n");
+            OutputDebugStringA("WM_DETROY\n");
         } break;
 
         case WM_CLOSE: {
             // PostQuitMessage(0);
             Running = false;
-            OutputDebugString("WM_CLOSE\n");
+            OutputDebugStringA("WM_CLOSE\n");
         } break;
 
         case WM_ACTIVATEAPP: {
-            OutputDebugString("WM_ACTIVATEAPP\n");
+            OutputDebugStringA("WM_ACTIVATEAPP\n");
         } break;
 
         case WM_SETCURSOR: {
-            SetCursor(customCursor);
+            SetCursor(0);
+        } break;
+
+        case WM_SYSKEYDOWN:
+        case WM_SYSKEYUP:
+        case WM_KEYUP:
+        case WM_KEYDOWN: {
+            #define WAS_DOWN_MASK (1 << 30)
+            #define IS_DOWN_MASK (1 << 31)
+            uint32_t VKCode = WParam;
+            bool WasDown = (LParam & WAS_DOWN_MASK) != 0;
+            bool IsDown = (LParam & IS_DOWN_MASK) == 0;
+
+            if(VKCode == 'W' && IsDown) {
+                global_game_state.ypos += 10;
+            }
+            if(VKCode == 'A' && IsDown) {
+                global_game_state.xpos -= 10;
+            }
+            if(VKCode == 'S' && IsDown) {
+                global_game_state.ypos -= 10;
+            }
+            if(VKCode == 'D' && IsDown) {
+                global_game_state.xpos += 10;
+            }
+            if(VKCode == VK_ESCAPE && (!WasDown) && IsDown) {
+                Running = false;
+            }
         } break;
 
         case WM_PAINT: {
            PAINTSTRUCT Paint;
            HDC DeviceContext = BeginPaint(Window, &Paint);
-           int x = Paint.rcPaint.left;
-           int y = Paint.rcPaint.top;
-           int width = Paint.rcPaint.right - Paint.rcPaint.left;
-           int height = Paint.rcPaint.bottom - Paint.rcPaint.top;
            RECT ClientRect;
+           win32_window_dimensions WinSize = Win32GetWindowSize(Window);
            GetClientRect(Window, &ClientRect);
-           Win32DisplayBufferWindow(DeviceContext, ClientRect, &GlobalBackBuffer, x, y, width, height);
+           Win32DisplayBufferWindow(DeviceContext, WinSize.width, WinSize.height, &GlobalBackBuffer);
            EndPaint(Window, &Paint);
-           OutputDebugString("WM_PAINT\n");
+           OutputDebugStringA("WM_PAINT\n");
         } break;
 
         default: {
@@ -146,91 +207,7 @@ LRESULT CALLBACK Win32MainWindowCallback(
 
 }
 
-internal HCURSOR makeCursor(HINSTANCE instance) {
-    // Yin-shaped cursor AND mask (32x32x1bpp)
-     BYTE ANDmaskCursor[] =
-    {
-        0xFF, 0xFC, 0x3F, 0xFF,   // ##############----##############
-        0xFF, 0xC0, 0x1F, 0xFF,   // ##########---------#############
-        0xFF, 0x00, 0x3F, 0xFF,   // ########----------##############
-        0xFE, 0x00, 0xFF, 0xFF,   // #######---------################
-        0xF8, 0x01, 0xFF, 0xFF,   // #####----------#################
-        0xF0, 0x03, 0xFF, 0xFF,   // ####----------##################
-        0xF0, 0x03, 0xFF, 0xFF,   // ####----------##################
-        0xE0, 0x07, 0xFF, 0xFF,   // ###----------###################
-        0xC0, 0x07, 0xFF, 0xFF,   // ##-----------###################
-        0xC0, 0x0F, 0xFF, 0xFF,   // ##----------####################
-        0x80, 0x0F, 0xFF, 0xFF,   // #-----------####################
-        0x80, 0x0F, 0xFF, 0xFF,   // #-----------####################
-        0x80, 0x07, 0xFF, 0xFF,   // #------------###################
-        0x00, 0x07, 0xFF, 0xFF,   // -------------###################
-        0x00, 0x03, 0xFF, 0xFF,   // --------------##################
-        0x00, 0x00, 0xFF, 0xFF,   // ----------------################
-        0x00, 0x00, 0x7F, 0xFF,   // -----------------###############
-        0x00, 0x00, 0x1F, 0xFF,   // -------------------#############
-        0x00, 0x00, 0x0F, 0xFF,   // --------------------############
-        0x80, 0x00, 0x0F, 0xFF,   // #-------------------############
-        0x80, 0x00, 0x07, 0xFF,   // #--------------------###########
-        0x80, 0x00, 0x07, 0xFF,   // #--------------------###########
-        0xC0, 0x00, 0x07, 0xFF,   // ##-------------------###########
-        0xC0, 0x00, 0x0F, 0xFF,   // ##------------------############
-        0xE0, 0x00, 0x0F, 0xFF,   // ###-----------------############
-        0xF0, 0x00, 0x1F, 0xFF,   // ####---------------#############
-        0xF0, 0x00, 0x1F, 0xFF,   // ####---------------#############
-        0xF8, 0x00, 0x3F, 0xFF,   // #####-------------##############
-        0xFE, 0x00, 0x7F, 0xFF,   // #######----------###############
-        0xFF, 0x00, 0xFF, 0xFF,   // ########--------################
-        0xFF, 0xC3, 0xFF, 0xFF,   // ##########----##################
-        0xFF, 0xFF, 0xFF, 0xFF    // ################################
-    };
 
-    // Yin-shaped cursor XOR mask (32x32x1bpp)
-    BYTE XORmaskCursor[] =
-    {
-        0x00, 0x01, 0x00, 0x00,   // --------------------------------
-        0x00, 0x03, 0xC0, 0x00,   // --------------####--------------
-        0x00, 0x3F, 0x00, 0x00,   // ----------######----------------
-        0x00, 0xFE, 0x00, 0x00,   // --------#######-----------------
-        0x03, 0xFC, 0x00, 0x00,   // ------########------------------
-        0x07, 0xF8, 0x00, 0x00,   // -----########-------------------
-        0x07, 0xF8, 0x00, 0x00,   // -----########-------------------
-        0x0F, 0xF0, 0x00, 0x00,   // ----########--------------------
-        0x1F, 0xF0, 0x00, 0x00,   // ---#########--------------------
-        0x1F, 0xE0, 0x00, 0x00,   // ---########---------------------
-        0x3F, 0xE0, 0x00, 0x00,   // --#########---------------------
-        0x3F, 0xE0, 0x00, 0x00,   // --#########---------------------
-        0x3F, 0xF0, 0x00, 0x00,   // --##########--------------------
-        0x7F, 0xF0, 0x00, 0x00,   // -###########--------------------
-        0x7F, 0xF8, 0x00, 0x00,   // -############-------------------
-        0x7F, 0xFC, 0x00, 0x00,   // -#############------------------
-        0x7F, 0xFF, 0x00, 0x00,   // -###############----------------
-        0x7F, 0xFF, 0x80, 0x00,   // -################---------------
-        0x7F, 0xFF, 0xE0, 0x00,   // -##################-------------
-        0x3F, 0xFF, 0xE0, 0x00,   // --#################-------------
-        0x3F, 0xC7, 0xF0, 0x00,   // --########----######------------
-        0x3F, 0x83, 0xF0, 0x00,   // --#######------#####------------
-        0x1F, 0x83, 0xF0, 0x00,   // ---######------#####------------
-        0x1F, 0x83, 0xE0, 0x00,   // ---######------####-------------
-        0x0F, 0xC7, 0xE0, 0x00,   // ----######----#####-------------
-        0x07, 0xFF, 0xC0, 0x00,   // -----#############--------------
-        0x07, 0xFF, 0xC0, 0x00,   // -----#############--------------
-        0x01, 0xFF, 0x80, 0x00,   // -------##########---------------
-        0x00, 0xFF, 0x00, 0x00,   // --------########----------------
-        0x00, 0x3C, 0x00, 0x00,   // ----------####------------------
-        0x00, 0x00, 0x00, 0x00,   // --------------------------------
-        0x00, 0x00, 0x00, 0x00    // --------------------------------
-    };
-
-    // Create a custom cursor at run time.
-
-    return CreateCursor(instance,   // app. instance
-                 16,                // horizontal position of hot spot
-                 16,                 // vertical position of hot spot
-                 32,                // cursor width
-                 32,                // cursor height
-                 ANDmaskCursor,     // AND mask
-                 XORmaskCursor );
-}
 
 int CALLBACK
 WinMain(HINSTANCE Instance, HINSTANCE PrevInstance, LPSTR lpCmdLine, int nCmdShow) {
@@ -241,7 +218,6 @@ WinMain(HINSTANCE Instance, HINSTANCE PrevInstance, LPSTR lpCmdLine, int nCmdSho
     windowClass.hInstance = Instance;
     // windowClass.hIcon = ;
     windowClass.lpszClassName = "HandmadeHeroWindowClass";
-    customCursor = makeCursor(Instance);
     if(RegisterClass(&windowClass)) {
         HWND windowHandle = CreateWindowEx(
             0,
@@ -270,7 +246,22 @@ WinMain(HINSTANCE Instance, HINSTANCE PrevInstance, LPSTR lpCmdLine, int nCmdSho
                     TranslateMessage(&message);
                     DispatchMessage(&message);
                 }
-                RenderGradient(GlobalBackBuffer, global_game_state.time, global_game_state.time*2);
+
+                for(DWORD ControllerIndex=0;ControllerIndex < XUSER_MAX_COUNT; ++ControllerIndex) {
+                    XINPUT_STATE ControllerState;
+                    if(XInputGetState(ControllerIndex, &ControllerState) == ERROR_SUCCESS) {
+                        XINPUT_GAMEPAD *Pad = &ControllerState.Gamepad;
+
+                        bool DPadUp = Pad->wButtons & XINPUT_GAMEPAD_DPAD_UP;
+                        bool DPadLeft = Pad->wButtons & XINPUT_GAMEPAD_DPAD_LEFT;
+                        bool DPadRight = Pad->wButtons & XINPUT_GAMEPAD_DPAD_RIGHT;
+                        bool DPadDown = Pad->wButtons & XINPUT_GAMEPAD_DPAD_DOWN;
+                    } else {
+                        // Not available
+                    }
+                }
+
+                RenderGradient(GlobalBackBuffer, global_game_state.xpos, global_game_state.ypos);
                 InvalidateRect(windowHandle, 0, FALSE);
             }
         }
