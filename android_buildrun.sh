26b714a8b495e9bf40fbd5fd5db8e8bd50374d48
Laszlo Korte <email@laszlokorte.de>
2026-02-04 02:14:42 +0100

    improve android build script

--- android_buildrun.sh
+++ android_buildrun.sh
@@ -2,6 +2,17 @@
 
 set -e
 
+# Build APK
 ./android_build.sh
+
+# show APK content
 unzip -l build/android/handmade-signed.apk
+
+# copy onto device
 adb install build/android/handmade-signed.apk
+
+# Start APK on device
+adb shell am start -n de.handmade.hero/android.app.NativeActivity
+
+# print debug log
+adb logcat *:E
